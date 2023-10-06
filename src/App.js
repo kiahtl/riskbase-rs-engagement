417596f24bb442508d25f0c9b422a58a017cfece
kiahtl <kiah@thousand-lines.com>
2023-10-06 12:54:35 +0100

    comment to test git set up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import Buildings from './components/buildings/buildings.component';
-import { ReactComponent as RiskbaseSvg } from './assets/svg/riskbase.svg';
+import "./App.css";
+import Buildings from "./components/buildings/buildings.component";
+import { ReactComponent as RiskbaseSvg } from "./assets/svg/riskbase.svg";
 
 function App() {
   return (
     <div className="App">
-      <div className='app-header'>      
-        <b >Resident Engagement</b> by <a href="https://riskbase.uk" target="_blank" rel='noreferrer'><RiskbaseSvg className='riskbase-logo'/></a>
+      <div className="app-header">
+        <b>Resident Engagement</b> by{" "}
+        <a href="https://riskbase.uk" target="_blank" rel="noreferrer">
+          <RiskbaseSvg className="riskbase-logo" />
+        </a>
       </div>
-      <div className='app-wrapper'>
+      <div className="app-wrapper">
         <Buildings />
-        
       </div>
     </div>
   );
 }
-
+// adding a comment to test git
 export default App;
